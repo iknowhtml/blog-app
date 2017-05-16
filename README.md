8354a28c7d5957c993685d5855bbbf3e3bea4d64
Aki Gao <xag9bb@virginia.edu>
2017-05-16 19:40:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Blog Application
+A simple blogging application built with React, Redux and React Router.
+
+#### To use
+Checkout this repo, install dependencies, then run the following commands in the terminal.
+
+```
+> git clone https://github.com/iknowhtml/react-redux-template.git
+> cd react-redux-template
+> yarn install
+> yarn start
+```
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: './src/index.jsx',
+
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  resolveLoader: {
+    moduleExtensions: ['-loader'],
+  },
+
+  devServer: {
+    contentBase: './',
+    port: 8080,
+  },
+};
