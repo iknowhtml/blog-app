a7ae8d652c439aeabad86ab92374da1e92513885
Aki Gao <xag9bb@virginia.edu>
2017-05-17 10:15:19 -0400

    complete back to redux - index action, add fetch posts action and creator

---
+++ src/actions/postsActionCreator.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import FETCH_POSTS from './postsActions';
+
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=AKIGAO';
+
+export default function fetchActions() {
+  const request = axios.get()`${ROOT_URL}/posts${API_KEY}`;
+  return {
+    type: FETCH_POSTS,
+    payload: request,
+  };
+}
---
+++ src/actions/postsActions.js
@@ -0,0 +1,2 @@
+const FETCH_POSTS = 'FETCH_POSTS';
+export default FETCH_POSTS;
