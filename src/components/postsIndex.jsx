aaa12d2d6f2110914a17389d3fe39d01e1361959
Aki Gao <xag9bb@virginia.edu>
2017-05-17 22:46:49 -0400

    complete nagivation with link component, add link to navigate to posts new from posts index

--- src/components/postsIndex.jsx
+++ src/components/postsIndex.jsx
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 import fetchPosts from '../actions/postsActionCreator';
 
 class PostsIndex extends Component {
@@ -20,6 +21,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Posts
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
