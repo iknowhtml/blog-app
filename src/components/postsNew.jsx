ae2ee178c8ef3a7ecd3c5e3a5a4699be451e691b
Aki Gao <xag9bb@virginia.edu>
2017-05-18 20:30:53 -0400

    complete more on navigation, add cancel navigation back to index

--- src/components/postsNew.jsx
+++ src/components/postsNew.jsx
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {field.meta.touched ? field.meta.error : ''}
+        </div>
       </div>
     );
   }
 
- onSubmit(value){
+  onSubmit(value) {
     console.log(value);
   }
 
   render() {
-    const {handleSubmit} = this.props;
+    const { handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field label="Title" name="title" component={this.renderField} />
         <Field label="Categories" name="categories" component={this.renderField} />
         <Field label="Content" name="content" component={this.renderField} />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
