da261da7ed65adf5c3f9fdbd753bf3b34fb67057
Aki Gao <xag9bb@virginia.edu>
2017-05-18 23:13:50 -0400

    complete receiving new posts, add fetch post action and reducer case, add babel preset stage-2 to dependencies

--- src/actions/postsActionCreator.js
+++ src/actions/postsActionCreator.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_POSTS, CREATE_POST } from './postsActions';
+import { FETCH_POST, FETCH_POSTS, CREATE_POST } from './postsActions';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=AKIGAO';
@@ -13,10 +13,17 @@ export function fetchPosts() {
 }
 
 export function createPost(values, callback) {
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values).
-  then(() => callback());
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values).then(() => callback());
   return {
     type: CREATE_POST,
+    payload: request,
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request,
   };
 }
