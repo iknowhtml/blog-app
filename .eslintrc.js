c3c103469719d34147e5a6ecdd6d12d49c112f80
Aki Gao <xag9bb@virginia.edu>
2017-05-20 10:32:23 -0400

    add is_prod and is_github_project as global variables to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb'],
+  globals: {
+    IS_PROD: true,
+    IS_GITHUB_PROJECT: true,
+  },
   rules: {
     'react/forbid-prop-types': [1, { forbid: ['any'] }],
     'react/prop-types': 0,
     'class-methods-use-this': [1, { exceptMethods: ['renderField'] }],
+    'func-names': ['error', 'as-needed'],
   },
   plugins: ['react', 'jsx-a11y', 'import', 'eslint-plugin-html'],
   env: {
--- src/helper/root_resolver.js
+++ src/helper/root_resolver.js
@@ -1,5 +1,2 @@
-/* global IS_PROD:true
-  global IS_GITHUB_PROJECT:true
-*/
 const root = IS_PROD && IS_GITHUB_PROJECT ? `/${window.location.pathname.split('/')[1]}` : '';
 export default root;
